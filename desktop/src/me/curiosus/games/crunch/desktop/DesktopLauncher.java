1ac2b97264761c7dde2dce9875e53c1ae0dbb7dd
curiosus <curiosus@gmail.com>
2019-08-26 18:54:23 -0600

    initial libgdx commit

---
+++ desktop/src/me/curiosus/games/crunch/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.curiosus.games.crunch.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.curiosus.games.crunch.CrunchGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CrunchGame(), config);
+	}
+}
